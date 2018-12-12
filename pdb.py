3aa1e246ec26aa02a7773e545982777fa263e724
Kirill Zinovjev <kzinovjev@gmail.com>
2018-12-12 12:14:11 +0000

    Create pdb module and tests

---
+++ pdb.py
@@ -0,0 +1,72 @@
+from itertools import groupby
+
+
+class Pdb(object):
+
+    def __init__(self, file=None, atoms=None):
+
+        if file is None and atoms is None:
+            raise ValueError('Either file or atoms must be provided')
+
+        if file is None:
+            self.atoms = atoms
+        # ignores everything that is not ATOM or HETATM
+        self.atoms = [parse_atom(line) for line in file
+                      if any(x in line[:6] for x in ['ATOM', 'HETATM'])]
+
+    @property
+    def residues(self):
+        """list of atom lists from each residue"""
+        return [list(v) for k, v in groupby(self.atoms, _residue_hash)]
+
+    def get_residues_by_name(self, residue_name):
+        return [residue for residue in self.residues
+                if residue[0]['resName'] == residue_name]
+
+    def to_file(self, file):
+        dump_atoms_to_file(file, self.atoms)
+
+
+def _residue_hash(atom):
+    """Uniquely identifies the residue atom belongs to"""
+    return '_'.join([str(atom[key])
+                     for key in ['chainID', 'resSeq', 'resName']])
+
+
+def parse_atom(atom_line):
+    """
+    Based on official PDB format from
+    http://www.wwpdb.org/documentation/file-format-content/format33/sect9.html
+    """
+    return {
+        'record': atom_line[:6].strip(),
+        'serial': int(atom_line[6:11].strip()),
+        'name': atom_line[12:16].strip(),
+        'altLoc': atom_line[16].strip(),
+        'resName': atom_line[17:20].strip(),
+        'chainID': atom_line[21].strip(),
+        'resSeq': int(atom_line[22:26]),
+        'iCode': atom_line[26].strip(),
+        'x': float(atom_line[30:38]),
+        'y': float(atom_line[38:46]),
+        'z': float(atom_line[46:54]),
+        'occupancy': float(atom_line[54:60]),
+        'tempFactor': float(atom_line[60:66]),
+        'element': atom_line[76:78].strip(),
+        'charge': atom_line[78:80].strip(),
+        'extras': atom_line[80:]
+    }
+
+
+def dump_atom(atom):
+    name_format = "{name:>4}" if len(atom['name']) > 2 else " {name:<3}"
+    return ("{record:6}{serial:5} " + name_format + "{altLoc:1}"
+            "{resName:>3} {chainID:1}{resSeq:4}{iCode:1}"
+            "   {x:8.3f}{y:8.3f}{z:8.3f}{occupancy:6.2f}"
+            "{tempFactor:6.2f}          {element:>2}"
+            "{charge:>2}{extras}").format(**atom)
+
+
+def dump_atoms_to_file(file, atoms):
+    for atom in atoms:
+        file.write(dump_atom(atom))
---
+++ tests/test_pdb.py
@@ -0,0 +1,56 @@
+import unittest
+from io import StringIO
+import pdb
+
+
+class TestPdb(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.atom_string = ("ATOM    278  HG2 ARG A  36      "
+                           "17.074  -2.417  17.463  1.00 11.39"
+                           "           H   new")
+        cls.parsed_atom = {
+            'record': "ATOM",
+            'serial': 278,
+            'name': "HG2",
+            'altLoc': "",
+            'resName': "ARG",
+            'chainID': "A",
+            'resSeq': 36,
+            'iCode': "",
+            'x': 17.074,
+            'y': -2.417,
+            'z': 17.463,
+            'occupancy': 1.0,
+            'tempFactor': 11.39,
+            'element': "H",
+            'charge': "",
+            'extras': " new"
+        }
+        with open('tests/full.pdb') as f:
+            cls.pdb = pdb.Pdb(f)
+
+    def test__residue_hash(self):
+        self.assertEqual(pdb._residue_hash(self.parsed_atom), "A_36_ARG")
+
+    def test_parse_atom(self):
+        self.assertEqual(pdb.parse_atom(self.atom_string), self.parsed_atom)
+
+    def test_dump_atom(self):
+        atom = pdb.parse_atom(self.atom_string)
+        self.assertEqual(pdb.dump_atom(self.parsed_atom), self.atom_string)
+
+    def test_create_pdb_from_file(self):
+        self.assertEqual(len(self.pdb.atoms), 2236)
+        self.assertEqual(len(self.pdb.residues), 463)
+
+    def test_dump_pdb_to_file(self):
+        result_file = StringIO()
+        self.pdb.to_file(result_file)
+        result_file.seek(0)
+        with open('tests/only_atoms.pdb', 'r') as pdb_file:
+            self.assertEqual(pdb_file.read(), result_file.read())
+
+    def test_get_residues_by_name(self):
+        self.assertEqual(len(self.pdb.get_residues_by_name('TRP')), 4)
