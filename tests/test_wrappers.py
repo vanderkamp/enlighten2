38f4cbb64c62e07e866c29dbe9c36b3ad6b86b58
Kirill Zinovjev <kzinovjev@gmail.com>
2018-12-17 14:35:14 +0000

    Implement Pdb4AmberReduceWrapper

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,6 +1,14 @@
 import unittest
 import unittest.mock as mock
 import wrappers
+import pdb
+
+
+def setup_mock(mock_os, mock_os_path):
+    mock_os_path.exists.return_value = False
+    mock_os_path.isfile.return_value = True
+    mock_os.environ = {'AMBERHOME': ""}
+    mock_os.getcwd.return_value = '.'
 
 
 class TestAntechamberWrapper(unittest.TestCase):
@@ -8,10 +16,7 @@ class TestAntechamberWrapper(unittest.TestCase):
     @mock.patch('wrappers.os.path')
     @mock.patch('wrappers.os')
     def test_antechamber_simple_call(self, mock_os, mock_os_path):
-        mock_os_path.exists.return_value = False
-        mock_os_path.isfile.return_value = True
-        mock_os.environ = {'AMBERHOME': ""}
-        mock_os.getcwd.return_value = '.'
+        setup_mock(mock_os, mock_os_path)
         antechamber = wrappers.AntechamberWrapper('XXX')
 
         mock_os.system.assert_has_calls([
@@ -21,3 +26,34 @@ class TestAntechamberWrapper(unittest.TestCase):
                       "-f prepc -o XXX.frcmod")
         ])
         mock_os_path.join.assert_called_with('.', 'XXX.frcmod')
+
+
+class TestPdb4AmberReduceWrapper(unittest.TestCase):
+
+    @mock.patch('wrappers.open')
+    @mock.patch('wrappers.os.path')
+    @mock.patch('wrappers.os')
+    def test_pdb4amber_reduce_call(self, mock_os, mock_os_path, mock_open):
+        setup_mock(mock_os, mock_os_path)
+        pdb4AmberReduce = wrappers.Pdb4AmberReduceWrapper(
+            system_name='XXX',
+            system_pdb='XXX_renamed_ligand.pdb'
+        )
+        mock_os.system.assert_has_calls([
+            mock.call("$AMBERHOME/bin/pdb4amber -i XXX_renamed_ligand.pdb "
+                      "-o XXX_pdb4amber.pdb --nohyd --dry &> pdb4amber.log"),
+            mock.call("$AMBERHOME/bin/reduce -build -nuclear "
+                      "XXX_pdb4amber.pdb &> XXX_reduce.pdb")
+        ])
+
+    def test_get_renamed_histidines(self):
+        renamed_histidines = {'A_262_HIS': 'HIE',
+                              'A_1_HIS': 'HIE',
+                              'A_71_HIS': 'HIE',
+                              'A_87_HIS': 'HID',
+                              'A_128_HIS': 'HIE',
+                              'A_133_HIS': 'HIE'}
+        with open('tests/test_files/reduce.pdb') as f:
+            reducePdb = pdb.Pdb(f)
+        self.assertEqual(wrappers.get_renamed_histidines(reducePdb),
+                         renamed_histidines)
--- wrappers.py
+++ wrappers.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import pdb
 
 
 def check_amberhome():
@@ -43,7 +44,7 @@ class AntechamberWrapper(object):
         )
 
         check_file(ligand_name + '.prepc',
-                "Antechamber failed to generate {ligand_name}.prepc file"
+                   "Antechamber failed to generate {ligand_name}.prepc file"
                    .format(ligand_name=ligand_name))
 
         if frcmod is None:
@@ -56,3 +57,50 @@ class AntechamberWrapper(object):
             self.frcmod = frcmod
 
         os.chdir('..')
+
+
+class Pdb4AmberReduceWrapper(object):
+
+    def __init__(self, system_name, system_pdb=None,
+                 working_directory=".pdb4amber_reduce"):
+
+        system_pdb = system_pdb or system_name + '.pdb'
+        check_file(system_pdb)
+        check_amberhome()
+        set_working_directory(working_directory)
+
+        os.system(
+            "$AMBERHOME/bin/pdb4amber "
+            "-i {system_pdb} -o {system_name}_pdb4amber.pdb "
+            "--nohyd --dry &> pdb4amber.log"
+            .format(system_pdb=system_pdb, system_name=system_name)
+        )
+        os.system(
+            "$AMBERHOME/bin/reduce -build -nuclear "
+            "{system_name}_pdb4amber.pdb &> {system_name}_reduce.pdb"
+            .format(system_name=system_name)
+        )
+        with open(system_name + '_reduce.pdb') as f:
+            reducePdb = pdb.Pdb(f)
+        self.renamed_histidines = get_renamed_histidines(reducePdb)
+
+        os.chdir('..')
+
+
+def get_renamed_histidines(reducePdb):
+    rename_dict = {'no HE2': 'HID',
+                   'no HD1': 'HIE',
+                   'bothHN': 'HIP'}
+    renamed_histidines = {}  # dict of residue_hash: new_name pairs
+    for line in reducePdb.other:
+        if line[:9] != 'USER  MOD' or line[25:28] != 'HIS':
+            continue
+        his_name = rename_dict.get(line[39:45])
+        if his_name is None:
+            continue
+        his_hash = pdb.residue_hash({'chainID': line[19],
+                                     'resSeq': int(line[20:24]),
+                                     'resName': "HIS"})
+        renamed_histidines[his_hash] = his_name
+
+    return renamed_histidines
