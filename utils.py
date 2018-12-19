8eaf75a46f5f028e14ff0f006b1c5a8d412d7bdd
Kirill Zinovjev <kzinovjev@gmail.com>
2018-12-19 14:50:44 +0000

    Implement utils.file_in_paths

--- utils.py
+++ utils.py
@@ -12,3 +12,11 @@ def set_working_directory(working_directory):
         shutil.rmtree(working_directory)
     os.makedirs(working_directory)
     os.chdir(working_directory)
+
+
+def file_in_paths(filename, path_list):
+    for path in path_list:
+        full_path = os.path.join(path, filename)
+        if os.path.isfile(full_path):
+            return full_path
+    return None
