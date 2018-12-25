7d75907bac2dd2cf89d91e6ed537011d91b2ab8d
Kirill Zinovjev <kzinovjev@gmail.com>
2018-12-25 12:59:04 +0000

    Implement tests for TleapWrapper and sphere template parser

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -2,6 +2,7 @@ import unittest
 import unittest.mock as mock
 import wrappers
 import pdb_utils
+from io import StringIO
 
 
 def iterable_mock_open(_mock=None, read_data=''):
@@ -130,3 +131,71 @@ class TestPropkaWrapper(unittest.TestCase):
         self.assertNotIn('A_189_ASP', residues)
         self.assertIn('A_189_ASH', residues)
         self.assertEqual(len(residues['A_189_ASH']), 12)
+
+
+class TestTleapWrapper(unittest.TestCase):
+
+    @mock.patch('wrappers.os.path.isfile')
+    def test_get_tleap_includes(self, mock_isfile):
+        result = {'loadoff test1/res1.off',
+                  'loadamberprep test1/res1.prepc',
+                  'loadamberprep test2/res2.prepc',
+                  'loadamberparams test1/res1.frcmod',
+                  'loadamberparams test2/res2.frcmod'}
+
+        FILES = {'test1': ['res1.off', 'res1.prepc', 'res1.frcmod'],
+                 'test2': ['res2.prepc', 'res2.frcmod']}
+
+        def side_effect(path):
+            dir, name = path.split('/')
+            return name in FILES[dir]
+        mock_isfile.side_effect = side_effect
+
+        self.assertEqual(
+            set(wrappers.get_tleap_includes(['test1', 'test2'],
+                                            ['res1', 'res2']).split('\n')),
+            result
+        )
+
+        with self.assertRaises(FileNotFoundError) as context:
+            wrappers.get_tleap_includes(['test1', 'test2'],
+                                        ['res1', 'res3'])
+        self.assertEquals(str(context.exception),
+                          "Cannot find topology (res3.prepc) "
+                          "for residue res3. Exiting...")
+
+    @mock.patch('wrappers.utils.set_working_directory')
+    @mock.patch('wrappers.os.system')
+    @mock.patch('wrappers.get_tleap_includes')
+    def test_tleap_call(self, mock_includes, *args):
+        pdb = mock.MagicMock()
+        pdb.tofile = mock.MagicMock()
+        PARAMS = {'name': 'XXX',
+                  'pdb': pdb,
+                  'water_pdb': pdb,
+                  'solvent_radius': 20.0,
+                  'solvent_closeness': 0.75,
+                  'ligand': [{'name': "CA",
+                              'resSeq': 1}]}
+        template_file = StringIO()
+        with open('tleap/sphere.in') as f:
+            template_file.write(f.read())
+        template_file.seek(0)
+
+        generated_input = StringIO()
+
+        def side_effect(data):
+            generated_input.write(data)
+
+        mock_includes.return_value = "loadoff XXX.off\nloadamberprep XXX.prepc"
+        with mock.patch("builtins.open", mock.mock_open()) as mock_file:
+            mock_input = mock.MagicMock()
+            mock_input.write = mock.MagicMock()
+            mock_input.write.side_effect = side_effect
+            mock_file.return_value.__enter__.side_effect = [template_file,
+                                                            mock_input]
+            with mock.patch("builtins.print", mock.mock_open()) as mock_print:
+                wrappers.TleapWrapper('sphere', params=PARAMS)
+        generated_input.seek(0)
+        with open('tests/test_files/sphere.in') as f:
+            self.assertEqual(generated_input.read(), f.read())
