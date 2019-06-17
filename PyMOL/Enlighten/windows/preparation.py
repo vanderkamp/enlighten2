300a1dd41bb166f5b794620577d3ae125475f2ed
Kirill Zinovjev <kzinovjev@gmail.com>
2019-06-17 14:23:55 +0100

    Add custom validator and directory mode to file selectors in preparation window

--- PyMOL/Enlighten/windows/preparation.py
+++ PyMOL/Enlighten/windows/preparation.py
@@ -7,6 +7,13 @@ class PreparationTab(ManagedWindow):
     def __init__(self, name, window_manager):
         path = os.path.join(os.path.dirname(__file__), 'preparation.ui')
         super().__init__(name, path, window_manager)
+        self.pdbFileEdit.set_validator(self.pdb_validator)
+        self.outputEdit.set_directory_mode(True)
+
+    @staticmethod
+    def pdb_validator(filename):
+        ext = os.path.splitext(filename)[-1]
+        return os.path.isfile(filename) and ext.lower() == '.pdb'
 
     def bind(self, controller):
         prep_advanced = self.window_manager['prep_advanced']
