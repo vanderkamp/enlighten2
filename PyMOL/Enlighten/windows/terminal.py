45336c3f2027acc40aa735433a997c055a0b8fed
Kirill Zinovjev <kzinovjev@gmail.com>
2019-06-25 17:05:52 +0100

    Implement terminal window

---
+++ PyMOL/Enlighten/windows/terminal.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtWidgets, uic
+
+
+class TerminalWindow(QtWidgets.QDialog):
+
+    def __init__(self, process, title):
+        super().__init__()
+        uic.loadUi('windows/terminal.ui', self)
+        self.setWindowTitle(title)
+        self.process = process
+        self.terminal.attach(process)
+
+    def closeEvent(self, event):
+        self.process.kill()
